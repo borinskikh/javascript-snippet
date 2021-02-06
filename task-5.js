268616b5e033ed15d315ceeecd7b3549e1f8c931
borinskikh <causelesscause@gmail.com>
2021-02-06 23:39:32 +0300

    update

--- task-5.js
+++ task-5.js
@@ -7,11 +7,12 @@ class Device {
 }
 
 class ElectronicDevice extends Device {
-    constructor(name, description, brand, battery = [false, false], pluggedIn = false, turnedOn = false) {
+    constructor(name, description, brand, battery = [false, false], powerConsumption = 0, pluggedIn = false, turnedOn = false) {
         super(name, description, brand);
-        this.battery = battery;
-        this.pluggedIn = pluggedIn;
-        this.turnedOn = turnedOn;
+        this.battery = battery; // [does it have a battery, is it charged]
+        this.powerConsumption = powerConsumption; // units of measurement - W/h
+        this.pluggedIn = pluggedIn; // true - plugged in, false - it's not plugged in
+        this.turnedOn = turnedOn; // true - turned on, false - turned off
     }
 
     plugIn() {
@@ -59,15 +60,14 @@ class ElectronicDevice extends Device {
 }
 
 class ComputerMonitor extends ElectronicDevice {
-    constructor(name, brand, size = 0, resolution = [0, 0], refreshRate = 0, pluggedIn, turnedOn) {
+    constructor(name, brand, size = 0, resolution = [0, 0], refreshRate = 0, powerConsumption, pluggedIn, turnedOn) {
         const description = 'A computer monitor is an output device that displays information in pictorial form. A monitor usually comprises the visual display, circuitry, casing, and power supply. The display device in modern monitors is typically a thin film transistor liquid crystal display with LED backlighting having replaced cold-cathode fluorescent lamp backlighting.';
         const battery = [false, false];
-        super(name, description, brand, battery, pluggedIn, turnedOn);
+        super(name, description, brand, battery, powerConsumption, pluggedIn, turnedOn);
         this.brand = brand;
-        this.size = size;
-        this.resolution = resolution;
-        this.refreshRate = refreshRate;
-        this.isPlugged = false;
+        this.size = size; // units of measurement - inch
+        this.resolution = resolution; // units of measurement - pixel
+        this.refreshRate = refreshRate; //units of measurement - Hz
     }
 }
 
@@ -76,12 +76,13 @@ class Speaker extends ElectronicDevice {
         const description = 'Speakers are transducers that convert electromagnetic waves into sound waves. The speakers receive audio input from a device such as a computer or an audio receiver. This input may be either in analog or digital form. Analog speakers simply amplify the analog electromagnetic waves into sound waves. Since sound waves are produced in analog form, digital speakers must first convert the digital input to an analog signal, then generate the sound waves.';
         super(name, description, brand, battery, pluggedIn, turnedOn);
         this.type = type;
-        this.soundVolume = soundVolume;
+        this.soundVolume = soundVolume; // units of measurement - dB
+        this.weight = weight; // units of measurement - kg
     }
 }
 
-const monitor = new ComputerMonitor(model = "27GN750-B", brand = "LG", size = 27, resolution = [1920, 1080], refreshRate = 240, pluggedIn = false, turnedOn = false);
-const speaker = new Speaker(model = "The New SOUNDBOKS (3rd Gen)", brand = "SOUNDBOKS", type = "Wireless", soundVolume = 126, weight = 15.4, battery = [true, true], pluggedIn = false, turnedOn = false);
+const monitor = new ComputerMonitor(model = "27GN750-B", brand = "LG", size = 27, resolution = [1920, 1080], refreshRate = 240, powerConsumption = 48, pluggedIn = false, turnedOn = false);
+const speaker = new Speaker(model = "The New SOUNDBOKS (3rd Gen)", brand = "SOUNDBOKS", type = "wireless", soundVolume = 126, weight = 15.4, battery = [true, true], powerConsumption = 99.84, pluggedIn = false, turnedOn = false);
 
 monitor.turnOn();
 speaker.turnOn();
